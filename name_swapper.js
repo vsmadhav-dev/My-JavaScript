fbbaa8cac9b8a900c6d63cb66595480b22cc4925
MADHAV ANAND <m47903237@gmail.com>
2026-05-29 14:55:03 +0530

    Just a quick bug fix.

--- name_swapper.js
+++ name_swapper.js
@@ -1,8 +1,8 @@
 function nameSwapper(name){
     let nameArray = name.split(' ');
-     let first_name = nameArray[1];
-     let last_name = nameArray[0];
-  let reversedName1 = first_name + " " +last_name;
+     let first_name = nameArray[0];
+     let last_name = nameArray[1];
+  let reversedName1 =last_name  + " " +first_name;
  
   return reversedName1;
 
