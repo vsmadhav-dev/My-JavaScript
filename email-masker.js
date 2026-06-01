0e6a84c74615d69db155e83917f2b5b994137120
MADHAV ANAND <m47903237@gmail.com>
2026-06-01 12:42:31 +0530

    Sorry! code was not saved before pushing.

--- email-masker.js
+++ email-masker.js
@@ -6,7 +6,7 @@ function maskEmail(email) {
     let rest = email.slice(numtomask);
     let finalEmail =  email[0] + maskedEmail + num + rest;
     return finalEmail; 
-     // return maskedEmail.length = 7 done for checking if that's working correct or not(optional).
+     // return maskedEmail.length; [= 7 done for checking if that's working correct or not(optional).]
    
 }
 console.log(maskEmail("m47903237@gmail.co.in"));
