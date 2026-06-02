cecd632c9570a9bc7182301f7e7310ef0eebda0e
MADHAV ANAND <m47903237@gmail.com>
2026-06-02 13:53:28 +0530

    A Record Manager in JavaScript which manages record such as adding , deleting etc.

---
+++ record-manger.js
@@ -0,0 +1,40 @@
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+function updateRecords(records , id , prop , value){
+
+  if(value === ""){
+    delete records[id][prop];
+   return records;  
+  }
+   if(prop !== "tracks" && value !== ""){
+     records[id][prop] = value;
+     return records;
+   }
+   if(!records[id].hasOwnProperty('tracks')){
+        records[id]["tracks"] = [];
+        
+        
+   }
+  records[id]["tracks"].push(value);
+
+   return records;
+        
+}
+
