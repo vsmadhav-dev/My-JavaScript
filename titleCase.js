88f98d76d398a116551254c9d9c5566854391413
MADHAV ANAND <m47903237@gmail.com>
2026-06-29 13:48:10 +0530

    A title case converter which capitalize the very first letter after space.

---
+++ titleCase.js
@@ -0,0 +1,13 @@
+function titleCase(str){
+  const split = str.split(" ");
+  let title;
+  let spfinal = "";
+  for(let i = 0; i < split.length; i++){
+  let sp = split[i].toLowerCase().split("");
+ sp[0] = sp[0].toUpperCase();
+   spfinal += sp.join("") + " ";
+   title = spfinal; 
+  }
+  return title.trim();
+
+}
