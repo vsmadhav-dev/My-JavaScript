c8eaecf3b6043da68729e8d6af3f82ecc4c7646c
MADHAV ANAND <m47903237@gmail.com>
2026-08-02 13:27:17 +0530

    Add truthCheck function to validate properties in an array of objects

---
+++ allTruthyValidator.js
@@ -0,0 +1,10 @@
+function truthCheck(collection, pre) {
+for(let i = 0; i < collection.length; i++){
+  if(!collection[i][pre]){
+    return false;
+  }
+}   
+ return true;
+}
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
