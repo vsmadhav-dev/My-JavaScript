dcb471d812f559fb6f9f2c926660203f58cad780
MADHAV ANAND <m47903237@gmail.com>
2026-08-03 09:38:32 +0530

    Add additional test cases for truthCheck function

--- allTruthyValidator.js
+++ allTruthyValidator.js
@@ -8,3 +8,4 @@ for(let i = 0; i < collection.length; i++){
 }
 
 console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "", role: "Bot", isBot: true}], "name"));
